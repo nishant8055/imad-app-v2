16a9e75a280c18c1b3da38b4502a4e7de34ebf1d
nishant8055 <nishantkumarsinghrathore@gmail.com>
2017-02-10 15:30:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/', function (req, res) {
 });
 app.get('/articleName',function(req,res){
 var articleName=req.params.articleName;
-    res.send(createTemplate[articleName]);
+    res.send(createTemplate(articles[articleName]));
 });
 app.get('/article-two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));

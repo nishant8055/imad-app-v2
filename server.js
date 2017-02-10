c136c85315ba877dff21e7494243b7af956fdbb3
nishant8055 <nishantkumarsinghrathore@gmail.com>
2017-02-10 13:08:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-    res.send('article one is requested and served');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
-    res.send('article two is requested and served');
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-three',function(req,res){
-    res.send('article three is requested and served');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

b6855e03fce2ae871447963e620627448b47e357
nishant8055 <nishantkumarsinghrathore@gmail.com>
2017-02-12 01:49:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,3 +16,16 @@ button.onclick=function()
        }
     };
 };
+//submit name
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function()
+{var names=['name1','name2','name3'];
+var list='';
+for(var i=0;i<names.length;i++)
+{
+    
+}
+    
+}

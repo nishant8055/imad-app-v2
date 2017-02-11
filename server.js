8ecf78012370f5c0659db9836957bace1bb0d246
nishant8055 <nishantkumarsinghrathore@gmail.com>
2017-02-12 02:02:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,13 @@ app.get('/counter',function(req,res)
    counter=counter+1;
    res.send(counter,toString());
 });
+var names=[];
+app.get('/submit-name',function(req,res)
+{
+    var name=req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 app.get('/:articleName',function(req,res){
 var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));

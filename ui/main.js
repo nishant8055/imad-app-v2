12620ba2fb79d0682cb7ae40908b704242beca68
nishant8055 <nishantkumarsinghrathore@gmail.com>
2017-02-12 18:25:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,6 +43,6 @@ submit.onclick=function()
     };
     var nameInput=document.getElementById('name');
     var name=nameInput.value;
-    request.open('GET','http://nishant8055.imad.hasura-app.io/submit-name?name=',true);
+    request.open('GET','http://nishant8055.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
 };

db162c562301c8e855519013cb266d3f5c8d17e8
nishant8055 <nishantkumarsinghrathore@gmail.com>
2017-02-12 16:10:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ var counter=0;
 app.get('/counter',function(req,res)
 {
    counter=counter+1;
-   res.send(counter,toString());
+   res.send(counter.toString());
 });
 var names=[];
 app.get('/submit-name',function(req,res)

ab4bd9a606ea4a2e960d07a2c426087330d63c6d
nishant8055 <nishantkumarsinghrathore@gmail.com>
2017-02-12 23:28:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ button.onclick=function()
             {
                 var counter=request.responseText;
                 var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
+                span.innerHTML=counter.toString('count');
             }
         }
     };

4ae14de228226440ef47a386f0bc8511b9aa36cf
nishant8055 <nishantkumarsinghrathore@gmail.com>
2017-02-13 17:41:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,16 +7,17 @@ button.onclick=function()
     //capture the response and save it in a variable
     request.onreadystatechanage=function()
     {
-        if(request.readyState==XMLHttpRequest.DONE){
-        //take some action
-        if(request.status==200)
+        if(request.readyState==XMLHttpRequest.DONE)
         {
-          var counter=request.responseText;
-          var span=document.getElementById('count');
-          span.innerHTML=counter.toString();  
+            //take some action
+             if(request.status==200)
+             {
+              var counter=request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();  
+             }
         }
-    }
-    //not done yet
+       //not done yet
     };
     request.open('GET','http://nishant8055.imad.hasura-app.io/counter',true);
     request.send(true);
